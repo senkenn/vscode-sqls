7956ff07f79934dd929edb7487c0b552243bb6e3
senkenn <senken32@gmail.com>
2024-03-03 19:53:41 +0900

    remove eslint and add biome, cspell, lefthook

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,45 +1,42 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext } from "vscode";
 
 import {
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-} from 'vscode-languageclient';
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+} from "vscode-languageclient/node";
 
-import vscode = require('vscode');
+import vscode = require("vscode");
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
 	const config = parseLanguageServerConfig();
-    let serverOptions: ServerOptions = {
-        command: "sqls",
+	const serverOptions: ServerOptions = {
+		command: "sqls",
 
-        args: [...config.flags],
-    };
+		args: [...config.flags],
+	};
 
-    let clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: 'file', language: 'sql', pattern: '**/*.sql' }],
-    };
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [
+			{ scheme: "file", language: "sql", pattern: "**/*.sql" },
+		],
+	};
 
-    client = new LanguageClient(
-        'languageServerExample',
-        serverOptions,
-        clientOptions,
-    );
-    client.start();
+	client = new LanguageClient(
+		"languageServerExample",
+		serverOptions,
+		clientOptions,
+	);
+	client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
 
 interface LanguageServerConfig {
@@ -47,20 +44,9 @@ interface LanguageServerConfig {
 }
 
 export function parseLanguageServerConfig(): LanguageServerConfig {
-	const sqlsConfig = getSqlsConfig();
+	const sqlsConfig = vscode.workspace.getConfiguration("sqls");
 	const config = {
-		flags: sqlsConfig['languageServerFlags'] || [],
+		flags: sqlsConfig.languageServerFlags || [],
 	};
 	return config;
-}
-
-export function getSqlsConfig(uri?: vscode.Uri): vscode.WorkspaceConfiguration {
-	if (!uri) {
-		if (vscode.window.activeTextEditor) {
-			uri = vscode.window.activeTextEditor.document.uri;
-		} else {
-			uri = null;
-		}
-	}
-	return vscode.workspace.getConfiguration('sqls', uri);
 }
